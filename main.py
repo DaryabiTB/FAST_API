6f7d888af7cb9929457b733532538e49b1e1010a
DaryabiTB <talibdaryabi@gmail.com>
2023-10-08 14:53:56 +0200

    create_post normally

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello end"}
 
 
+@app.get("/hello/{name}")
+async def hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+class Post:
+    title: str
+
+
+@app.post("/create_post")
+def create_post(post: dict = Body(...)):
+    return {"message": f"created post: {post['title']}"}
